f45bf41ff096a136b6955d70f78388a0173cba84
Toni de la Fuente <toni@blyx.com>
2019-12-17 23:35:27 +0100

    Added new format as test

---
+++ arsenal-kreator.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+while IFS=';' read NAME USERANDREPO URL DESC; do 
+   echo "| **$NAME** | [https://github.com/$USERANDREPO](https://github.com/$USERANDREPO) | ${DESC} |[![stars](https://badgen.net/github/stars/$USERANDREPO)](https://badgen.net/github/stars/$USERANDREPO)| [![contributors](https://badgen.net/github/contributors/$USERANDREPO)](https://badgen.net/github/contributors/$USERANDREPO)|[![watchers](https://badgen.net/github/watchers/$USERANDREPO)](https://badgen.net/github/watchers/$USERANDREPO)|[![last-commit](https://badgen.net/github/last-commit/$USERANDREPO)](https://badgen.net/github/last-commit/$USERANDREPO) | [![open-issues](https://badgen.net/github/open-issues/$USERANDREPO)](https://badgen.net/github/open-issues/$USERANDREPO) |[![closed-issues](https://badgen.net/github/closed-issues/$USERANDREPO)](https://badgen.net/github/closed-issues/$USERANDREPO) |"
+done < arsenal.csv
